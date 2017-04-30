e964cb633a489692135f5a377c1b0df354ca3def
dddotcom <dddotcom@gmail.com>
2017-04-29 22:05:17 -0700

    added some text to make it look less off center

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var app = angular.module('PokedexAngularApp', []);
 
 app.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.pokemon = {};
+  $scope.error = {};
   $scope.pokemonSpecies = {};
   $scope.searchTerm
   $scope.search = function() {
@@ -17,14 +18,17 @@ app.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
 
     $http(req).then(function success(res){
       console.log(res.data);
+      $scope.error = {};
       $scope.pokemon = res.data;
       $http(req2).then(function success(res2){
         console.log(res2.data);
         $scope.pokemonSpecies = res2.data;
       }, function error(res2){
+        $scope.error = res2.data;
         console.log('error', res2);
       });
     }, function error(res){
+      $scope.error = res.data;
       console.log("error", res);
     });
   };
