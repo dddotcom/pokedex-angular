78544b9ae0ac4687ab62874b348dcd755ad3084d
dddotcom <dddotcom@gmail.com>
2017-05-02 10:01:54 -0700

    made local storage variable more unique

--- js/services.js
+++ js/services.js
@@ -18,27 +18,27 @@ angular.module('PokedexServices', [])
 .factory('Favorite', ["$window", function($window){
   return {
     add: function(id, pokemon, pokemonSpecies){
-      temp = $window.localStorage["favorites"] ? JSON.parse($window.localStorage["favorites"]) : {}
+      temp = $window.localStorage["favoritePokemon"] ? JSON.parse($window.localStorage["favoritePokemon"]) : {}
       if( !(id in temp) ){
         temp[id] = {};
         temp[id]["pokemon"] = pokemon;
         temp[id]["pokemonSpecies"] = pokemonSpecies;
-        $window.localStorage["favorites"] = JSON.stringify(temp);
+        $window.localStorage["favoritePokemon"] = JSON.stringify(temp);
       }
       return this.get();
     },
     get: function(){
-      if($window.localStorage["favorites"]){
-        return JSON.parse($window.localStorage["favorites"]);
+      if($window.localStorage["favoritePokemon"]){
+        return JSON.parse($window.localStorage["favoritePokemon"]);
       } else {
         return {};
       }
     },
     delete: function(id){
-      if($window.localStorage["favorites"]){
-          var temp = JSON.parse($window.localStorage["favorites"]);
+      if($window.localStorage["favoritePokemon"]){
+          var temp = JSON.parse($window.localStorage["favoritePokemon"]);
           delete temp[id];
-          $window.localStorage["favorites"] = JSON.stringify(temp);
+          $window.localStorage["favoritePokemon"] = JSON.stringify(temp);
       }
       return this.get();
     }
